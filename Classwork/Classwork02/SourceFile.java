607359465a3747321d7806e1d34081c096bcd34d
alvinlai808 <35281838+alvinlai808@users.noreply.github.com>
2018-09-05 22:17:00 -0700

    added variables, started openFile() method

--- Classwork/Classwork02/SourceFile.java
+++ Classwork/Classwork02/SourceFile.java
@@ -1,8 +1,20 @@
 package copiersupport;
 
 import java.io.FileReader;
-import java.io.IOException;
+import java.util.Scanner;
 
 public class SourceFile {
+
+	Scanner sc;
+	String fileName;
+	FileReader in = null;
+
+	public void openFile() {
+
+		System.out.print( "Enter the name of the file you want to open: ");
+		fileName = sc.nextLine();
+		// System.out.println( "user input: " + fileName );
+		// in = new FileReader( fileName );
+	}
 	
 }
