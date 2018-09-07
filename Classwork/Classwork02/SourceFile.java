3e38000b2b58a619ad47fcb97189b5cca4976592
alvinlai808 <35281838+alvinlai808@users.noreply.github.com>
2018-09-07 01:27:03 -0700

    Fixed indentation

--- Classwork/Classwork02/SourceFile.java
+++ Classwork/Classwork02/SourceFile.java
@@ -6,18 +6,18 @@ import java.io.IOException;
 
 public class SourceFile {
 
-	private String content;
-	
-	public SourceFile( String fileName ) {
-		try {
-			BufferedReader br = new BufferedReader( new FileReader( fileName ) );
-			content = br.readLine();
-		} catch( IOException ioe ) {
+   private String content;
 
-		}
-	}
+   public SourceFile( String fileName ) {
+      try {
+         BufferedReader br = new BufferedReader( new FileReader( fileName ) );
+         content = br.readLine();
+      } catch ( IOException ioe ) {
 
-	public String getContent() {
-		return content;
-	}
+      }
+   }
+
+   public String getContent() {
+      return content;
+   }
 }
