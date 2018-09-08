ca5e0f0c8b4eb01b3b315e9aaada2f673307d2ab
alvinlai808 <35281838+alvinlai808@users.noreply.github.com>
2018-09-07 18:36:30 -0700

    Added header and comments

--- Classwork/Classwork02/SourceFile.java
+++ Classwork/Classwork02/SourceFile.java
@@ -1,3 +1,7 @@
+/*
+ * Opens and retrieves content of source file specified by user
+ */
+
 package copiersupport;
 
 import java.io.BufferedReader;
@@ -6,12 +10,15 @@ import java.io.IOException;
 
 public class SourceFile {
 
-   private String content;
+   private String content = "";
+   private BufferedReader br;
 
+   // Opens file, reads and saves content to a String
    public SourceFile( String fileName ) {
       try {
-         BufferedReader br = new BufferedReader( new FileReader( fileName ) );
+         br = new BufferedReader( new FileReader( fileName ) );
          content = br.readLine();
+         br.close();
       } catch ( IOException ioe ) {
 
       }
