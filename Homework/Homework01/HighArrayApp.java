065d1a6c2657aefcd96804df06f97347a05b93c2
alvinlai808 <35281838+alvinlai808@users.noreply.github.com>
2018-09-19 10:50:00 -0700

    Added tests
    noDups not working properly :'(

--- Homework/Homework01/HighArrayApp.java
+++ Homework/Homework01/HighArrayApp.java
@@ -19,35 +19,101 @@ public class HighArrayApp {
       arr.insert(66);
       arr.insert(33);
 
-      // added duplicates for test case
-      arr.insert(77);
-      arr.insert(99);
-
       // test getMax()
+      System.out.println( "TESTING getMax() METHOD");
+      
       arr.display();
-      arr.getMax();
+      System.out.println( arr.getMax() );
+      System.out.println( "Adding values..." );
+      arr.insert(100);
+      arr.insert(1);
+      arr.insert(101);
+      arr.insert(150);
+      arr.insert(59);
+      arr.insert(200);
+      arr.display();
+      System.out.println( arr.getMax() );
+      System.out.println( "getMax() TEST COMPLETE");
       System.out.println( "---------------------------------" );
 
-      // test noDups()
-      System.out.println( "Testing noDups()" );
-      arr.noDups();
+      // test delete()
+      System.out.println( "TESTING delete() METHOD");
+
+      arr.delete(77);
+      arr.delete(99);
+      arr.delete(44);
+      arr.delete(55);
+      arr.delete(22);
+      arr.delete(88);
+      arr.delete(11);
+      arr.delete(00);
+      arr.delete(66);
+      arr.delete(33);
+      arr.delete(101);
+      arr.delete(150);
+      arr.delete(59);
+      arr.delete(200);
+      arr.delete(100);
       arr.display();
+      System.out.println( "delete() TEST COMPLETE");
+      System.out.println( "---------------------------------" );
 
-      int searchKey = 35;
+      // test find()
+      System.out.println( "TESTING find() METHOD"); 
 
+      int searchKey = 1;
+      System.out.println( "Search Key = " + searchKey );
       if( arr.find( searchKey ) ) {
          System.out.println( "Found " + searchKey );
       } else {
          System.out.println( "Can't find " + searchKey );
       }
 
-      arr.delete(00);
-      arr.delete(55);
-      arr.delete(99);
-      System.out.println( "---------------------------------" );
+      searchKey = 4;
+      System.out.println( "Search Key = " + searchKey );
+      if( arr.find( searchKey ) ) {
+         System.out.println( "Found " + searchKey );
+      } else {
+         System.out.println( "Can't find " + searchKey );
+      }
 
-      // test getMax();
+      searchKey = 6;
+      System.out.println( "Search Key = " + searchKey );
+      if( arr.find( searchKey ) ) {
+         System.out.println( "Found " + searchKey );
+      } else {
+         System.out.println( "Can't find " + searchKey );
+      }
+
+      System.out.println( "Deleting value: " + searchKey );
+      if( arr.find( searchKey ) ) {
+         System.out.println( "Found " + searchKey );
+      } else {
+         System.out.println( "Can't find " + searchKey );
+      }
+
+      System.out.println( "find() TEST COMPLETE");
+      System.out.println( "---------------------------------" );       
+
+      // test noDups()
+      System.out.println( "TESTING noDups() METHOD");  
+      System.out.println( "Adding values..." );    
+      arr.insert(1);
+      arr.insert(1);
+      arr.insert(2);
+      arr.insert(2);
+      arr.insert(2);
+      arr.insert(3);
+      arr.insert(4);
+      arr.insert(4);
+      arr.insert(5);
+      arr.insert(5);
+      arr.insert(6);
       arr.display();
-      arr.getMax();
+      arr.noDups();
+      arr.display();
+      System.out.println( "noDups() TEST COMPLETE");
+      System.out.println( "---------------------------------" );      
+
    }
 }
